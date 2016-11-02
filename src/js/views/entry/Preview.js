e893b3c811dd669c22dcfa48905d6326bf08128f
sawada <sawadasuiren@gmail.com>
2016-11-02 15:57:15 +0900

    プレビューにアイキャッチを設定するフォームを設置

--- src/js/views/entry/Preview.js
+++ src/js/views/entry/Preview.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {_} from 'lodash';
 import marked from 'marked';
 import ReactHtmlParser from 'react-html-parser';
+import ImageModal from './ImageModal';
+import ApiPath from '../../enum/ApiParam';
 
 /**
  * プレビュークラスです。
@@ -15,6 +17,9 @@ export default class Preview extends React.Component {
   constructor(props) {
     super(props);
 
+    this._onClickAddEyeCatch = this._onClickAddEyeCatch.bind(this);
+    this._onCancelEyeCatch = this._onCancelEyeCatch.bind(this);
+    this._onDecisionEyeCatch = this._onDecisionEyeCatch.bind(this);
     marked.setOptions({
       renderer: new marked.Renderer(),
       gfm: true,
@@ -27,6 +32,7 @@ export default class Preview extends React.Component {
     });
 
     this.state = {
+      activeEyeCatchModal: false,
       entryData: props.entryData
     };
   }
@@ -44,7 +50,7 @@ export default class Preview extends React.Component {
    * 記事内容が書き換わった際のハンドラーです。
    */
   _onChangeContent() {
-    let dom = this.refs.preview;
+    let dom = this.refs.entryContent;
     dom.innerHTML = marked(this.state.entryData.content);
     let codeItems = dom.getElementsByTagName('code');
     _.each(codeItems, (item) => {
@@ -58,7 +64,67 @@ export default class Preview extends React.Component {
   render() {
     return (
       <div className="preview" ref="preview">
+        <ImageModal
+          active={this.state.activeEyeCatchModal}
+          onCancel={this._onCancelEyeCatch}
+          onDecision={this._onDecisionEyeCatch}
+        />
+
+        <div className="entryEyeCatch" onClick={this._onClickAddEyeCatch}>
+          <p className="addText" ref="addText">
+            <i title="アイキャッチを追加" className="fa fa-eye fa-fw"></i> アイキャッチを追加
+          </p>
+          <img src="" className="eyeCatchImage" ref="eyeCatchImage" alt="" />
+        </div>
+        <div className="entryContent" ref="entryContent"></div>
       </div>
     );
+  }
+
+  /**
+   * アイキャッチ追加ボタン押下時のハンドラーです。
+   */
+  _onClickAddEyeCatch() {
+    this.setState({
+      activeEyeCatchModal: true
+    });
+  }
+
+  /**
+   * アイキャッチモーダルのキャンセルボタン押下時の
+   * ハンドラーです。
+   */
+  _onCancelEyeCatch() {
+    this.setState({
+      activeEyeCatchModal: false
+    });
+  }
+
+  /**
+   * アイキャッチモーダルの決定ボタン押下時の
+   * ハンドラーです。
+   */
+  _onDecisionEyeCatch(path) {
+    // 記事データを更新
+    let imagePath = ApiPath.getImagePath() + path;
+    let entryData = this.state.entryData;
+    entryData.eyeCatch = imagePath;
+
+    // アイキャッチエリアに画像をセット
+    this._setEyeCatch(imagePath);
+
+    this.setState({
+      activeEyeCatchModal: false,
+      entryData: entryData
+    });
+  }
+
+  /**
+   * アイキャッチエリアに画像を設定します。
+   */
+  _setEyeCatch(path) {
+    this.refs.eyeCatchImage.setAttribute('src', path);
+    this.refs.eyeCatchImage.style.display = 'block';
+    this.refs.addText.style.display = 'none';
   }
 }
