f6caf9f70968d084ac58eb5ea78b20e5d47ef008
Yoshikawa Ryota <yoshikawa@rrreeeyyy.com>
2016-11-02 10:18:27 +0900

    Add test initial

---
+++ src/js/__test__/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Login from '../views/login/Login.js';
+
+describe('App', () => {
+  it('should render login page in initial state', () => {
+    expect(1).toBe(1);
+  });
+})
